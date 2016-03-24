02f44d1646aec947256577bad466a69fdd2701d6
Jesús Navarrete <jesus.navarrete@holidayinsider.com>
2016-03-24 15:56:17 +0100

    reading the complete message

--- src/main/java/org/sciodb/server/ServerSocket.java
+++ src/main/java/org/sciodb/server/ServerSocket.java
@@ -2,8 +2,6 @@ package org.sciodb.server;
 
 import org.apache.log4j.Logger;
 import org.sciodb.server.services.Dispatcher;
-import org.sciodb.utils.CommandEncoder;
-import org.sciodb.utils.models.Command;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -23,6 +21,7 @@ public class ServerSocket implements Runnable {
 
     final static private Logger logger = Logger.getLogger(ServerSocket.class);
 
+    final static private int HEADER_SIZE = 4;
     private Selector selector;
     private Map<SocketChannel, List> dataMapper;
     private InetSocketAddress listenAddress;
@@ -71,7 +70,7 @@ public class ServerSocket implements Runnable {
                         accept(key);
                     }
                     else if (key.isReadable()) {
-                        read(key);
+                        readBuffer(key);
                     }
                 }
             }
@@ -97,29 +96,161 @@ public class ServerSocket implements Runnable {
 
     //read from the socket channel OLD VERSION
     private void read(final SelectionKey key) throws IOException {
-        final SocketChannel channel = (SocketChannel) key.channel();
-        final ByteBuffer buffer = ByteBuffer.allocate(1024);
-        int numRead = channel.read(buffer);
+        logger.info("size of the pool --> " + dataMapper.size());
+        try {
 
-        if (numRead == -1) {
-            dataMapper.remove(channel);
-            Socket socket = channel.socket();
-            SocketAddress remoteAddr = socket.getRemoteSocketAddress();
-            logger.info("Connection closed by client: " + remoteAddr);
-            channel.close();
-            key.cancel();
-            return;
+            final SocketChannel channel = (SocketChannel) key.channel();
+
+            final List<String> messages = new ArrayList<>();
+
+            while (true) {
+                final ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+                int bufferSize = channel.read(buffer);
+
+                if (bufferSize == -1) {
+                    dataMapper.remove(channel);
+                    Socket socket = channel.socket();
+                    SocketAddress remoteAddr = socket.getRemoteSocketAddress();
+                    logger.info("Connection closed by client: " + remoteAddr);
+                    channel.close();
+                    key.cancel();
+                    return;
+                }
+
+                byte[] data = new byte[bufferSize];
+                System.arraycopy(buffer.array(), 0, data, 0, bufferSize);
+
+                final String msg = new String(data);
+
+                if (msg != null && msg.length() > 0) {
+                    logger.info("Size: " + msg);
+                    int size = Integer.valueOf(msg);
+                    final ByteBuffer messageBuffer = ByteBuffer.allocate(size);
+                    bufferSize = channel.read(messageBuffer);
+
+                    if (bufferSize == -1) {
+                        dataMapper.remove(channel);
+                        Socket socket = channel.socket();
+                        SocketAddress remoteAddr = socket.getRemoteSocketAddress();
+                        logger.info("Connection closed by client: " + remoteAddr);
+                        channel.close();
+                        key.cancel();
+                        return;
+                    }
+
+                    data = new byte[bufferSize];
+                    System.arraycopy(messageBuffer.array(), 0, data, 0, bufferSize);
+
+                    logger.info("Got: " + new String(data));
+                    messages.add(new String(data));
+                }
+
+            }
+        } catch (Exception e) {
+            logger.error("There was an error reading the buffer: " + e.getLocalizedMessage());
         }
 
-        byte[] data = new byte[numRead];
-        System.arraycopy(buffer.array(), 0, data, 0, numRead);
-        logger.info("Got: " + new String(data));
 
-        final Command command = CommandEncoder.decode(new String(data));
+//
+//        final Command command = CommandEncoder.decode(""); //new String(data));
+//
+//        // do it asynchronously
+//        if (command != null && command.getOperationID() != null) {
+//            dispatcher.getService(command);
+//        }
 
-        // do it asynchronously
-        dispatcher.getService(command);
+    }
 
+    public void readBuffer(final SelectionKey key) {
+        try {
+            final SocketChannel channel = (SocketChannel) key.channel();
+
+                int msgSize = readHeader(channel);
+
+                if (msgSize == 0) {
+                    key.cancel();
+                } else {
+                    final String message = readMessage(channel, msgSize);
+//                    if (msgSize != message.length()) {
+//                        logger.info("Size: " + msgSize);
+//
+//                    }
+//                        logger.info(" message --> " + message);
+                }
+
+        } catch (Exception e) {
+            logger.error("There was an error reading the buffer: " + e.getLocalizedMessage());
+        }
+    }
+
+    private int readHeader(final SocketChannel channel) {
+        final ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+        int bufferSize = 0;
+        try {
+            bufferSize = channel.read(buffer);
+
+            if (bufferSize == -1) {
+                dataMapper.remove(channel);
+                final Socket socket = channel.socket();
+                final SocketAddress remoteAddr = socket.getRemoteSocketAddress();
+                logger.info("Connection closed by client: " + remoteAddr);
+                channel.close();
+                bufferSize = 0;
+            } else {
+                final byte[] data = new byte[bufferSize];
+                System.arraycopy(buffer.array(), 0, data, 0, bufferSize);
+
+                final String msg = new String(data);
+
+                if (msg != null && msg.length() > 0) {
+//                    logger.info("Size: " + msg);
+                    bufferSize = Integer.valueOf(msg);
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Error reading socket channel", e);
+        }
+        return bufferSize;
+
+    }
+
+    private String readMessage(final SocketChannel channel, int msgSize) throws IOException {
+        final StringBuilder result = new StringBuilder();
+        int total = 0;
+        if (msgSize > 0) {
+            boolean repeat = true;
+            int counter = 0;
+            while (repeat) {
+
+                final ByteBuffer messageBuffer = ByteBuffer.allocate(msgSize);
+                int currentSize = channel.read(messageBuffer);
+
+                if (currentSize == -1) {
+                    dataMapper.remove(channel);
+                    Socket socket = channel.socket();
+                    SocketAddress remoteAddr = socket.getRemoteSocketAddress();
+                    logger.info("Connection closed by client: " + remoteAddr);
+                    channel.close();
+//                    key.cancel();
+//                    return;
+                }
+
+                byte [] data = new byte[currentSize];
+                System.arraycopy(messageBuffer.array(), 0, data, total, currentSize);
+                total += currentSize;
+
+                final String str = new String(data);
+//                if (str.length() < msgSize) {
+//                    logger.info("Got: " + str);
+//                }
+                result.append(str);
+                if (total == msgSize) {
+                    repeat = false;
+                }
+
+            }
+        }
+        return result.toString();
     }
 
 }
