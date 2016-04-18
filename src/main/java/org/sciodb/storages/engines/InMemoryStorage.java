58805adf515208d6a51d6e3873900e589f0da5c9
Jesús Navarrete <jesus.navarrete@holidayinsider.com>
2016-04-18 15:55:28 +0200

    fixing compilation problems :(

--- src/main/java/org/sciodb/storages/engines/InMemoryStorage.java
+++ src/main/java/org/sciodb/storages/engines/InMemoryStorage.java
@@ -1,6 +1,7 @@
 package org.sciodb.storages.engines;
 
 import org.sciodb.storages.StorageEngine;
+import org.sciodb.utils.StorageException;
 
 import java.util.List;
 
@@ -8,6 +9,36 @@ import java.util.List;
  * @author jesus.navarrete  (24/03/16)
  */
 public class InMemoryStorage implements StorageEngine {
+
+    @Override
+    public void init() throws StorageException {
+
+    }
+
+    @Override
+    public void createDatabase(String name) throws StorageException {
+
+    }
+
+    @Override
+    public void createCollection(String name) throws StorageException {
+
+    }
+
+    @Override
+    public void persist(byte[] key, byte[] value) {
+
+    }
+
+    @Override
+    public byte[] find(byte[] key) {
+        return new byte[0];
+    }
+
+    @Override
+    public void close() {
+
+    }
 
     @Override
     public void create() {
--- src/main/java/org/sciodb/storages/impl/RocksDBEngine.java
+++ src/main/java/org/sciodb/storages/impl/RocksDBEngine.java
@@ -8,6 +8,8 @@ import org.rocksdb.RocksDBException;
 import org.sciodb.storages.StorageEngine;
 import org.sciodb.utils.StorageException;
 
+import java.util.List;
+
 /**
  * Implementation of RocksDB storage engine
  *
@@ -123,5 +125,50 @@ public class RocksDBEngine implements StorageEngine {
         if (db != null) db.close();
         // TODO look for the right method to kill the C++ stuff
         options.dispose();
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void createDatabase(byte[] database) {
+
+    }
+
+    @Override
+    public byte[] databaseInfo() {
+        return new byte[0];
+    }
+
+    @Override
+    public void createCollection(byte[] collection) {
+
+    }
+
+    @Override
+    public void dropCollection() {
+
+    }
+
+    @Override
+    public List<byte[]> query(byte[] query) {
+        return null;
+    }
+
+    @Override
+    public List<byte[]> bulkOperation(byte[] query) {
+        return null;
+    }
+
+    @Override
+    public List<byte[]> getIndexes() {
+        return null;
+    }
+
+    @Override
+    public void getStatistics() {
+
     }
 }
