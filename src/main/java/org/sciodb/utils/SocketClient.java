1ab3b271ad3b9748b9930e2dbb2ea13044d20466
Jesús Navarrete <jesus.navarrete@holidayinsider.com>
2016-07-07 22:52:07 +0200

    cleanning socket client, removing logs

--- src/main/java/org/sciodb/utils/SocketClient.java
+++ src/main/java/org/sciodb/utils/SocketClient.java
@@ -3,9 +3,6 @@ package org.sciodb.utils;
 import org.apache.log4j.Logger;
 import org.sciodb.exceptions.CommunicationException;
 import org.sciodb.messages.Message;
-import org.sciodb.messages.impl.Header;
-import org.sciodb.messages.impl.Node;
-import org.sciodb.messages.impl.NodeMessage;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -21,31 +18,9 @@ public class SocketClient {
 
     final static private Logger logger = Logger.getLogger(SocketClient.class);
 
-    public static void main(String[] args) {
-        final NodeMessage message = new NodeMessage();
-        final Header h = new Header();
-        h.setLength(0);
-        h.setOperationId(31);
-        h.setId("id");
-        message.setHeader(h);
-
-        final Node node = new Node();
-        node.setRole("chuncker");
-        node.setPort(9096);
-        node.setHost("host");
-        message.setNode(node);
-
-        try {
-            SocketClient.sendToSocket("127.0.0.1", 9092, message, false);
-        } catch (CommunicationException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static byte[] sendToSocket(final String host, final int port, final Message message, final boolean responseRequired) throws CommunicationException {
 
         final byte[] input = message.encode();
-        logger.debug("message ----- " + message.toString());
 
         final InetSocketAddress hostAddress = new InetSocketAddress(host, port);
 
@@ -54,11 +29,10 @@ public class SocketClient {
         try (SocketChannel client = SocketChannel.open(hostAddress)) {
             final String headerSize = String.format("%04d", input.length);
             final ByteBuffer header = ByteBuffer.wrap(headerSize.getBytes());
-            client.write(header);
-            logger.debug("header - " + new String(header.array()));
             final ByteBuffer buffer = ByteBuffer.wrap(input);
+
+            client.write(header);
             client.write(buffer);
-            logger.debug("buffer - " + new String(buffer.array()));
 
             buffer.clear();
             byte[] data;
@@ -73,7 +47,6 @@ public class SocketClient {
                 data = new byte[0];
             }
             client.close();
-            logger.debug(" data ");
             return data;
 
         } catch (IOException e) {
