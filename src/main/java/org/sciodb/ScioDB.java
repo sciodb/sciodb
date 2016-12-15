4158f4f820a7e03bec9b143fd7b83ec0705e9f07
Jesús Navarrete <jesus.navarrete@gmail.com>
2016-12-15 20:23:50 +0100

    Implementing a simple runtime hook

--- src/main/java/org/sciodb/ScioDB.java
+++ src/main/java/org/sciodb/ScioDB.java
@@ -34,8 +34,6 @@ public class ScioDB {
 
         final ScioDB scio = new ScioDB();
         scio.commandLine(args);
-
-        logger.info("Stopped!");
     }
 
     public void commandLine(final String[] args) {
@@ -101,8 +99,19 @@ public class ScioDB {
         try {
             new Thread(new ServerSocket(null, node.getPort())).start();
             new Thread(new TopologyRunnable(node, seeds)).start();
+
+            final Runtime runtime = Runtime.getRuntime();
+            runtime.addShutdownHook(new Thread(new Shutdown()));
+
         } catch (Exception e) {
             logger.error("Impossible to start the database", e);
+        }
+    }
+
+    private class Shutdown implements Runnable {
+        @Override
+        public void run() {
+            logger.info("Stopped!");
         }
     }
 }
