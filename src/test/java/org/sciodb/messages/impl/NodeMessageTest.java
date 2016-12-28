a9048feadc309de57c70ca5736a898fac9b7da0f
Jesús Navarrete <jesus.navarrete@gmail.com>
2016-12-28 13:08:44 +0100

    testing bytes size of 128 nodes

--- src/test/java/org/sciodb/messages/impl/NodeMessageTest.java
+++ src/test/java/org/sciodb/messages/impl/NodeMessageTest.java
@@ -2,8 +2,13 @@ package org.sciodb.messages.impl;
 
 import org.junit.Test;
 import org.sciodb.utils.GUID;
+import org.sciodb.utils.ScioDBConstants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Jesús Navarrete (28/06/16)
@@ -12,13 +17,12 @@ public class NodeMessageTest {
 
     @Test
     public void encode() throws Exception {
-        final NodeMessage nm = new NodeMessage();
 
-        final Node node = new Node();
-        node.setHost("host");
-        node.setPort(200);
+        final Node node = new Node("host", 200);
         final String guid = GUID.get();
         node.setGuid(guid);
+
+        final NodeMessage nm = new NodeMessage();
         nm.setNode(node);
 
         assertEquals(20 + guid.length(), node.encode().length);
@@ -33,6 +37,27 @@ public class NodeMessageTest {
         assertEquals(node.getHost(), result.getNode().getHost());
         assertEquals(node.getPort(), result.getNode().getPort());
         assertEquals(guid, result.getNode().getGuid());
+    }
+
+    @Test
+    public void encode_listOfNodesSize() {
+        int max = 128;
+
+        final List<Node> nodes = new ArrayList<>();
+        for (int i = 0; i < max; i++) {
+            final Node node = new Node("123.123.123.123", 9000 + i);
+            node.setGuid(GUID.get());
+
+            nodes.add(node);
+        }
+
+        final NodesMessage nodesMessage = new NodesMessage();
+        nodesMessage.getNodes().addAll(nodes);
+
+        byte[] bytes = nodesMessage.encode();
+//        System.out.println(bytes.length);
+        assertTrue( bytes.length < ScioDBConstants.MAX_ANSWER_BYTES);
+
     }
 
     @Test
