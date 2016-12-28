cea25be0ece9d699c7cce642d6b6ab459af18390
Jesús Navarrete <jesus.navarrete@gmail.com>
2016-12-28 19:44:57 +0100

    deleting not used methods

--- src/main/java/org/sciodb/exceptions/StorageException.java
+++ src/main/java/org/sciodb/exceptions/StorageException.java
@@ -5,24 +5,12 @@ package org.sciodb.exceptions;
  */
 public class StorageException extends Exception {
 
-    public StorageException() {
-        super();
-    }
-
     public StorageException(String message) {
         super(message);
     }
 
     public StorageException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public StorageException(Throwable cause) {
-        super(cause);
-    }
-
-    protected StorageException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
