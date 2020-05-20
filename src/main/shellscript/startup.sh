b5835dd9b7bdf19162231d3fc9d559222d675624
Jesús Navarrete <jesus.navarrete@gmail.com>
2020-05-20 20:33:58 +0200

    Moved scripts used to test sciodb to src/main/shellscript/
    Signed-off-by: Jesús Navarrete <jesus.navarrete@gmail.com>

--- src/main/shellscript/startup.sh
+++ src/main/shellscript/startup.sh
@@ -14,18 +14,18 @@
 export JAVA_OPTS_SCIODB="-Xms128m -Xmx256m -XX:MaxMetaspaceSize=128m"
 
 export CLASSPATH=".";
-for i in ../lib/*.jar ; do
+for i in ../../../lib/*.jar ; do
   CLASSPATH=$CLASSPATH:$i
 done
 
-CLASSPATH=$CLASSPATH:../target/classes/
+CLASSPATH=$CLASSPATH:../../../target/classes/
 seeds=$2
 if [ -z "$seeds" ]
 then
   echo "[WARNING] There is no seeds present"
-  nohup java $JAVA_OPTS_SCIODB -cp $CLASSPATH org.sciodb.ScioDB -p $1 > logs/node_$1.log 2>&1 &
+  java $JAVA_OPTS_SCIODB -cp $CLASSPATH org.sciodb.ScioDB -p $1
 else
-  nohup java $JAVA_OPTS_SCIODB -cp $CLASSPATH org.sciodb.ScioDB -p $1 -s $2 > logs/node_$1.log 2>&1 &
+  java $JAVA_OPTS_SCIODB -cp $CLASSPATH org.sciodb.ScioDB -p $1 -s $2
 fi
 
 pid=$!
--- src/main/shellscript/stop.sh
+++ src/main/shellscript/stop.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-#TODO change this mechanism
 kill -9 $(cat pids/node_$1.pid)
 rm pids/node_$1.pid
